a13db29e4ad0f9a686275e0f85c8d81451b0b834
John Raymond Wold <john.raymond.wold@nav.no>
2026-03-05 14:00:59 +0100

    Spring Initializr generert

---
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
@@ -0,0 +1,13 @@
+package no.nav.teamarbeidsforhold.githubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamArbeidsforholdStatistikkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.teamarbeidsforhold.githubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamArbeidsforholdStatistikkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
