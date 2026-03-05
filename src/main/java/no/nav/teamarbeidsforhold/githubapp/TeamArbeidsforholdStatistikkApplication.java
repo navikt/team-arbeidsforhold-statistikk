4d3af02bff68c7a98b7897c8b39e16639a883c30
John Raymond Wold <john.raymond.wold@nav.no>
2026-03-05 14:41:25 +0100

    Spring Initializr generert, div oppsett

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeamArbeidsforholdStatistikkApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
 	}
 
