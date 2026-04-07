f4d28a65ae3a4843581cd291a6f842fee670e993
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-07 11:10:37 +0200

    Bytt ut logging med enda mer fundamental logging

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
@@ -3,9 +3,14 @@ package no.nav.teamarbeidsforhold.githubapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class TeamArbeidsforholdStatistikkApplication {
 	static void main(String[] args) {
+		final Map<String, String> miljø = System.getenv();
+		System.out.println(String.join(",", miljø.keySet()));
 		SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
 	}
 }
