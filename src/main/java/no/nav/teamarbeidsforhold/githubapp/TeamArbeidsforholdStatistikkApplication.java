495087c2acdfddf6e6e70c4009af075106c29713
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-07 11:25:12 +0200

    Smalere logging

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Gatherers;
 
 @SpringBootApplication
 public class TeamArbeidsforholdStatistikkApplication {
-	static void main(String[] args) {
-		final Map<String, String> miljø = System.getenv();
-		System.out.println(String.join(",", miljø.keySet()));
-		SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
-	}
+    static void main(String[] args) {
+        final Map<String, String> miljø = System.getenv();
+        miljø.keySet().stream().filter(s -> s.toLowerCase().contains("pg")).gather(Gatherers.windowFixed(20)).forEach(System.out::println);
+        SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
+    }
 }
