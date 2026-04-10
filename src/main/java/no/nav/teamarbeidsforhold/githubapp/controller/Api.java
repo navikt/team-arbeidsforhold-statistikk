aed525b7ce215161aca616a65a40818064e2e2df
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-10 11:30:38 +0200

    Legg til openapi, sørg for at API er markert som RestController

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Api.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Api.java
@@ -7,12 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class Api {
     private final GitHub github;
