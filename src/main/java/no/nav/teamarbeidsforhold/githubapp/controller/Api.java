24dbc02127d383496a3f55fc8390862916256b70
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-10 18:55:24 +0200

    Generer spring boot rest-annotasjoner

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Api.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Api.java
@@ -4,28 +4,29 @@ import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTeam;
 import org.kohsuke.github.GitHub;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import no.nav.teamarbeidsforhold.githubapp.generert.api.ReposApi;
+import no.nav.teamarbeidsforhold.githubapp.generert.modell.Repo;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
-public class Api {
+public class Api implements ReposApi {
     private final GitHub github;
 
     Api(final GitHub github) {
         this.github = github;
     }
 
-    @GetMapping("/repoer")
-    public ResponseEntity<?> get() throws IOException {
-        final GHTeam team = github.getMyTeams().values().stream().findAny().orElseThrow().stream().findAny().orElseThrow();
-        final List<GHRepository> repoer = team.listRepositories().toList();
-        return ResponseEntity.ok("repoer: " + repoer.stream().map(GHRepository::getName).collect(Collectors.joining(", ")));
+    public ResponseEntity<List<Repo>> reposGet() {
+        try {
+            final GHTeam team = github.getMyTeams().values().stream().findAny().orElseThrow().stream().findAny().orElseThrow();
+            final List<GHRepository> repoer = team.listRepositories().toList();
+            return ResponseEntity.ok(repoer.stream().map(GHRepository::getName).map(r -> new Repo(r, "", 0)).toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
