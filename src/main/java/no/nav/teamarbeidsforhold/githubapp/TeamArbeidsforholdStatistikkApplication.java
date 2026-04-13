e54e7f51193be7ed937e61738634dc042a96220a
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-13 08:38:48 +0200

    Fjernet debug-kode ikke lenger relevant etter at postgresql fungerer

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/TeamArbeidsforholdStatistikkApplication.java
@@ -10,8 +10,6 @@ import java.util.stream.Gatherers;
 @SpringBootApplication
 public class TeamArbeidsforholdStatistikkApplication {
     static void main(String[] args) {
-        final Map<String, String> miljø = System.getenv();
-        miljø.keySet().stream().filter(s -> s.toLowerCase().contains("pg")).gather(Gatherers.windowFixed(20)).forEach(System.out::println);
         SpringApplication.run(TeamArbeidsforholdStatistikkApplication.class, args);
     }
 }
