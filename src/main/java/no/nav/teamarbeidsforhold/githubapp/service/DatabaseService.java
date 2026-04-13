28668d5edaddcee315b043b0f3c91403c55c9830
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-13 12:42:13 +0200

    Hard-koding av repoer for nå

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/service/DatabaseService.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/service/DatabaseService.java
@@ -18,6 +18,60 @@ public class DatabaseService {
     }
 
     public List<Repo> findAll() {
-        return repoRepository.gyldigeRepoer().stream().map(repoEntitet -> new Repo(repoEntitet.getFullName(), repoEntitet.getCloneUrl(), 0)).toList();
+        final String repoer = """
+                navikt/aareg
+                navikt/aareg-ajourholdsfiler
+                navikt/aareg-aktor-sync
+                navikt/aareg-analyse
+                navikt/aareg-aura
+                navikt/aareg-batch
+                navikt/aareg-dist-api
+                navikt/aareg-dist-api-contract
+                navikt/aareg-dist-mottak
+                navikt/aareg-dist-mottak-contract
+                navikt/aareg-dist-online
+                navikt/aareg-dist-online-api
+                navikt/aareg-dist-sync
+                navikt/aareg-dist-sync-endringer
+                navikt/aareg-dolly-api
+                navikt/aareg-gamle-arbeidsgivernavn
+                navikt/aareg-innsyn-saksbehandler
+                navikt/aareg-melding-pusher
+                navikt/aareg-meldingsbehandling
+                navikt/aareg-meldingsdefinisjon
+                navikt/aareg-metrics-pusher
+                navikt/aareg-otp-api
+                navikt/aareg-services
+                navikt/aareg-services-contract
+                navikt/aareg-status
+                navikt/aareg-teknisk-historikk
+                navikt/aareg-tenor-adapter
+                navikt/aareg-test-container
+                navikt/aareg-tilgangskontroll
+                navikt/aareg-tjenestespesifikasjoner
+                navikt/aareg-uttrekk
+                navikt/aareg-uttrekk-eksternt
+                navikt/aareg-vedlikehold
+                navikt/abac-aareg-core
+                navikt/abac-brreg-proxy
+                navikt/abac-registre-aareg
+                navikt/arbeid-og-inntekt
+                navikt/arbeidsgiver-innsyn-aareg
+                navikt/arbeidsgiver-innsyn-aareg-api
+                navikt/brreg-proxy
+                navikt/dataprodukt-register-aareg
+                navikt/dataprodukt-register-ereg
+                navikt/ereg-aura
+                navikt/ereg-services
+                navikt/ereg-solr-enhetssok
+                navikt/nav-api-portal
+                navikt/nav-maskinporten
+                navikt/team-arbeidsforhold-github-runner
+                navikt/team-arbeidsforhold-statistikk
+                navikt/team-arbeidsforhold-utvikling
+                navikt/vault-iac
+                """;
+        return Arrays.stream(repoer.split("\\s+")).map(navn -> new Repo(navn, "https://github.com/navikt/" + navn, 0)).toList();
+        //return repoRepository.gyldigeRepoer().stream().map(repoEntitet -> new Repo(repoEntitet.getFullName(), repoEntitet.getCloneUrl(), 0)).toList();
     }
 }
