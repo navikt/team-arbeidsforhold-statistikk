b688b85f3aaa5d170e4b109f3c66194d9cfec4fb
John Raymond Wold <john.raymond.wold@nav.no>
2026-04-15 07:17:49 +0200

    Debug litt for å finne ut type klasse fra Spring Security

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Bruker.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/controller/Bruker.java
@@ -2,22 +2,22 @@ package no.nav.teamarbeidsforhold.githubapp.controller;
 
 import no.nav.teamarbeidsforhold.githubapp.generert.modell.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
 
-@RestController("")
+@RestController()
+@RequestMapping("/api")
 public final class Bruker {
-    @RequestMapping(
-            method = RequestMethod.GET,
-            value = "/me",
-            produces = {"application/json"}
-    )
-    public ResponseEntity<User> getBruker(@AuthenticationPrincipal UserDetails bruker) {
-        return ok(new User(bruker.getUsername()));
+    @RequestMapping(method = RequestMethod.GET, value = "/me", produces = {"application/json"})
+    public ResponseEntity<User> getBruker(Authentication bruker) {
+        final User user = new User(bruker.getName());
+        if (bruker.getPrincipal() != null) {
+            user.setDebug("Klasse: " + bruker.getPrincipal().getClass());
+        }
+        return ok(user);
     }
 }
