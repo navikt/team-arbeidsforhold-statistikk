f71e0ffc86de0199dd1287bcca1923921b2b9d69
John Raymond Wold <john.raymond.wold@nav.no>
2026-05-05 09:12:31 +0200

    WIP på sårbarheter

---
+++ src/main/aksel-frontend/src/components/CriticalVulnerabilities.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@navikt/ds-react";
+export function CriticalVulnerabilities() {
+    return (
+        <Heading size="small" level="2">
+            Kritiske sårbarheter
+        </Heading>
+    );
+}
