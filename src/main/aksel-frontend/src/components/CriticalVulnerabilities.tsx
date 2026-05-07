98b79412dc6e56875007315a6408788b2e1d9970
John Raymond Wold <john.raymond.wold@nav.no>
2026-05-07 08:12:52 +0200

    Mer arbeid på sårbarhets-komponent

--- src/main/aksel-frontend/src/components/CriticalVulnerabilities.tsx
+++ src/main/aksel-frontend/src/components/CriticalVulnerabilities.tsx
@@ -1,8 +1,81 @@
-import { Heading } from "@navikt/ds-react";
+import {BodyShort, ErrorMessage, Heading, HStack, Loader, Tag, VStack} from "@navikt/ds-react";
+
+import {useEffect, useState} from "react";
+import {VulnerabilityApi} from "../api.ts";
+import type {Vulnerability} from "../api.ts";
+import axios, {type AxiosResponse} from "axios";
+
+const api = new VulnerabilityApi(
+    undefined,
+    undefined,
+    axios.create({
+        baseURL: window.location.origin,
+    })
+);
+
 export function CriticalVulnerabilities() {
+    const [vulnerabilities, setVulnerabilities] = useState<Vulnerability[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        api.apiVulnerabilitiesCriticalGet({})
+            .then((response: AxiosResponse<Vulnerability[]>) => {
+                setVulnerabilities(response.data);
+            })
+            .catch((err: unknown) => {
+                setError("Failed to load critical vulnerabilities");
+                console.error(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <Loader/>;
+    if (error) return <ErrorMessage>{error}</ErrorMessage>;
+
+    if(vulnerabilities.length == 0) return (
+        <VStack>
+            <Heading level="2" size="small">
+                Kritiske sårbarheter
+            </Heading>
+
+            <HStack align="center">
+                <Tag variant="success" size="xsmall">
+                    Ingen sårbarheter!
+                </Tag>
+                <BodyShort>
+                    Ingen nye kritiske sårbarheter ble funnet
+                </BodyShort>
+            </HStack>
+
+            <BodyShort textColor="subtle">
+                Godt jobba!
+            </BodyShort>
+        </VStack>
+    );
+
     return (
-        <Heading size="small" level="2">
-            Kritiske sårbarheter
-        </Heading>
+        <VStack>
+            <Heading size="small" level="2">
+                Kritiske sårbarheter
+            </Heading>
+
+            {vulnerabilities.map((vuln) => (
+                <HStack key={vuln.id} align="center">
+                    <Tag variant="error" size="xsmall">
+                        Kritisk sårbarhet
+                    </Tag>
+                    <BodyShort>
+                        {vuln.id}
+                    </BodyShort>
+                    <BodyShort textColor="subtle">
+                        I {vuln.affectedRepositories} repoer
+                    </BodyShort>
+                </HStack>
+            ))}
+
+        </VStack>
     );
 }
