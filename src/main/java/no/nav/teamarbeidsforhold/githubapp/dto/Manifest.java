55f42c90dc216b16e27111f33e1ef4d483a27fe2
John Raymond Wold <john.raymond.wold@nav.no>
2026-07-10 09:52:09 +0200

    La til litt fabrikk-metoder for å gjøre nais-api typene litt mer håndterlige

--- src/main/java/no/nav/teamarbeidsforhold/githubapp/dto/Manifest.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/dto/Manifest.java
@@ -1,4 +1,15 @@
 package no.nav.teamarbeidsforhold.githubapp.dto;
 
 public record Manifest(String content) {
+    private static Manifest of(final String type, final String jobname) {
+        return new Manifest("apiVersion: nais.io/v1alpha1\nkind: " + type + "\nmetadata:\n  labels:\n    team: arbeidsforhold\n  name: " + jobname + "\n");
+    }
+
+    public static Manifest job(final String jobname) {
+        return of("Naisjob", jobname);
+    }
+
+    public static Manifest application(final String appname) {
+        return Manifest.of("Application", appname);
+    }
 }
--- src/main/java/no/nav/teamarbeidsforhold/githubapp/dto/TeamEnvironment.java
+++ src/main/java/no/nav/teamarbeidsforhold/githubapp/dto/TeamEnvironment.java
@@ -1,4 +1,7 @@
 package no.nav.teamarbeidsforhold.githubapp.dto;
 
 public record TeamEnvironment(Environment environment) {
+    public static TeamEnvironment of(final String name) {
+        return new TeamEnvironment(new Environment(name));
+    }
 }
