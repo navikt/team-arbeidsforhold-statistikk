be43b3984d9619f88b0f2df9fd42829898572934
John Raymond Wold <john.raymond.wold@nav.no>
2026-08-06 16:14:28 +0200

    Oppdaterte admin-side med advarsel, la til knapp for nais-oppdatering

--- src/main/aksel-frontend/src/pages/AdminPage.tsx
+++ src/main/aksel-frontend/src/pages/AdminPage.tsx
@@ -1,4 +1,4 @@
-import {Button} from "@navikt/ds-react";
+import {Button, ErrorSummary} from "@navikt/ds-react";
 import {AdminApi} from "../api.ts";
 import axios from "axios";
 import {useState} from "react";
@@ -13,11 +13,17 @@ const api = new AdminApi(
 
 export default function AdminPage() {
     const [cveLoading, setCveLoading] = useState(false);
+    const [naisLoading, setNaisLoading] = useState(false);
     const [liquibaseLoading, setLiquibaseLoading] = useState(false);
     const handleCveClick = async (): Promise<void> => {
         setCveLoading(true);
         await api.apiAdminNvdjobbPost();
         setCveLoading(false);
+    }
+    const handleNaisClick = async (): Promise<void> => {
+        setNaisLoading(true);
+        await api.apiAdminNaisjobbPost();
+        setNaisLoading(false);
     }
     const handleLiquibaseClick = async (): Promise<void> => {
         setLiquibaseLoading(true);
@@ -27,8 +33,15 @@ export default function AdminPage() {
 
     return (
         <>
+            <ErrorSummary heading="Vær grei og ikke trykk på noen av disse knappene hvis du ikke vet nøyaktig hva du gjør.">
+                <ErrorSummary.Item>Databasen kan komme i en tilstand hvor appen ikke fungerer.</ErrorSummary.Item>
+                <ErrorSummary.Item>Eksterne leverandører kan blokkerer oss for å ha gjort for mange API-kall.</ErrorSummary.Item>
+            </ErrorSummary>
             <Button data-color="danger" variant="primary" onClick={handleCveClick} loading={cveLoading}>
                 Kjør CVE-jobb
+            </Button>
+            <Button data-color="danger" variant="primary" onClick={handleNaisClick} loading={naisLoading}>
+                Oppdater data fra Nais
             </Button>
             <Button data-color="danger" variant="primary" onClick={handleLiquibaseClick} loading={liquibaseLoading}>
                 Nullstill lås på liquibase
