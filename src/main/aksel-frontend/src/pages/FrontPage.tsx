7ec9cc3d358ecf54af5488046c06e7e5574fa0ac
John Raymond Wold <john.raymond.wold@nav.no>
2026-08-10 09:48:27 +0200

    Gjør forside bittelitt mer presentabel

--- src/main/aksel-frontend/src/pages/FrontPage.tsx
+++ src/main/aksel-frontend/src/pages/FrontPage.tsx
@@ -1,22 +1,12 @@
-import { List } from "@navikt/ds-react";
+import {BodyShort} from "@navikt/ds-react";
 import {CriticalVulnerabilities} from "../components/CriticalVulnerabilities.tsx";
-
-const items = [
-    "First item",
-    "Second item",
-    "Third item",
-];
 
 export default function FrontPage() {
     return (
         <>
             <CriticalVulnerabilities/>
 
-            <List size="small">
-                {items.map((item, index) => (
-                    <List.Item key={index}>{item}</List.Item>
-                ))}
-            </List>
+            <BodyShort>Denne siden er under konstruksjon, foreløpig er det kun widget-en over med nylige kritiske sårbarheter.</BodyShort>
         </>
     );
 }
